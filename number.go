3ddd29b87966539a2514b3213a6dc223e899d7ee
Victor <ddvictorbb@naver.com>
2023-03-15 17:13:58 +0900

    Initial implementation

--- number.go
+++ number.go
@@ -1,1 +1,27 @@
 package number
+
+import "golang.org/x/exp/constraints"
+
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max[T constraints.Ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Bound[T constraints.Ordered](v, min, max T) T {
+	if v < min {
+		return min
+	}
+	if v > max {
+		return max
+	}
+	return v
+}
